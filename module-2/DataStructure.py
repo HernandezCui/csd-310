2ac38fd15a5bb91ac6659fe65208fdc1722e8817
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-11-03 10:36:06 -0800

    Adding Data models assignment

---
+++ module-2/DataStructure.py
@@ -0,0 +1,16 @@
+{
+  "user": {
+    "user_id": "1",
+    "name": "John Doe",
+    "email": "john@example.com",
+    "birthdate": "1985-01-01",
+    "roles": [
+      { "role_id": "1", "role_name": "Admin" },
+      { "role_id": "2", "role_name": "User" }
+    ],
+    "dependents": [
+      { "dependent_id": "1", "dependent_name": "Jane Doe", "relationship": "Daughter" },
+      { "dependent_id": "2", "dependent_name": "Jimmy Doe", "relationship": "Son" }
+    ]
+  }
+}
